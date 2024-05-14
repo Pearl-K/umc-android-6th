9f4b69a22ff8d78e060954b1ccdf690d74a2ef9c
Pearl <rkdwlswn227@gmail.com>
2024-05-14 11:00:41 +0900

    add: week5

--- week4/week1_flo/app/src/main/java/com/example/week1_flo/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.week1_flo
-
-data class Song(
-    val title : String = "",
-    val singer : String = "",
-    val second : Int = 0,
-    val playTime : Int = 0,
-    var isPlaying : Boolean = false
-)
